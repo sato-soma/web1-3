b2ab336dc64eea026269b00fcc2701547b9ed557
サトウ　ソウマ <souma.1005@icloud.com>
2018-05-07 17:56:38 +0900

    Auto stash before merge of "master" and "origin/master"

--- Sample/Assets/Test.cs
+++ Sample/Assets/Test.cs
@@ -2,10 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+<<<<<<< Updated upstream
 public class Test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+=======
+public class Player
+{
+
+    private int hp = 100;
+    private int power = 50;
+    
+    public void Attack()
+    {
+        Debug.Log(this.power + "のダメージを与えた");
+    }
+
+    public void Damage(int damage)
+    {
+        this.hp -= damage;
+        Debug.Log(damage + "のダメージを受けた");
+    }
+}
+
+
+
+
+
+public class Test : MonoBehaviour {
+
+
+    void SayHello()
+    {
+        Debug.Log("Hello");
+    }
+
+    void Callname(string name)
+    {
+        Debug.Log("Hello " + name);
+    }
+
+    int Add(int a, int b)
+    {
+        int c = a + b;
+
+        return c;
+    }
+
+    // Use this for initialization
+    void Start () {
+>>>>>>> Stashed changes
         Debug.Log("Hello, World");
 
         int age;
@@ -113,6 +160,58 @@ public class Test : MonoBehaviour {
             sum += i;
         }
          Debug.Log(sum);
+<<<<<<< Updated upstream
+=======
+
+        int[] array = new int[5];
+
+        array[0] = 2;
+        array[1] = 10;
+        array[2] = 5;
+        array[3] = 15;
+        array[4] = 3;
+
+        for (int i = 0; i < 5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        int[] points = { 83, 99, 52, 93, 15 };
+
+        for (int i = 0; i < points.Length; i++)
+        {
+            if(points[i] >= 90)
+            {
+                Debug.Log(points[i]);
+            }
+        }
+
+        SayHello();
+
+        Callname("Tom");
+
+        
+        answer = Add(2, 3);
+        Debug.Log(answer);
+
+        Player myPlayer = new Player();
+        myPlayer.Attack();
+        myPlayer.Damage(30);
+
+        Vector2 playerPos = new Vector2(3.0f, 4.0f);
+        playerPos.x += 8.0f;
+        playerPos.y += 5.0f;
+        Debug.Log(playerPos);
+
+        Vector2 startPos = new Vector2(2.0f, 1.0f);
+        Vector2 endPos = new Vector2(8.0f, 5.0f);
+        Vector2 dir = endPos - startPos;
+        Debug.Log(dir);
+
+        float len = dir.magnitude;
+        Debug.Log(len);
+
+>>>>>>> Stashed changes
     }
 	
 	// Update is called once per frame
